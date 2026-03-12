1f8318ab56e582fba3a4094399a58100728ccfdb
Nick-Kevin <nicblou23@gmail.com>
2026-03-13 03:01:34 +0400

    update : result oo the form

---
+++ src/components/GaugeChart.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Doughnut } from "react-chartjs-2";
+import annotationPlugin from 'chartjs-plugin-annotation';
+import { Chart as ChartJS } from "chart.js";
+
+ChartJS.register(annotationPlugin);
+
+const GaugeChart: React.FC = (): React.JSX.Element => {
+    const VALUE: number = 45;
+
+    return (
+        <>
+            <Doughnut
+                data={{
+                    datasets: [{
+                        data: [VALUE, 100-VALUE],
+                        backgroundColor: ['#3b82f6', 'rgb(234, 234, 234)'],
+                        borderWidth: 0,
+                        borderRadius: [
+                            { outerStart: 23, innerStart: 23 },
+                            { outerEnd: 23, innerEnd: 23 }
+                        ],
+                    }]
+                }}
+                options={{
+                    aspectRatio: 2,
+                    circumference: 180,
+                    rotation: -90,
+                    cutout: '80%',
+                    scales: {
+                        y: {
+                            type: 'linear', // Explicitly set the type to 'linear'
+                            min: 0,         // Optional: define the minimum value for the scale
+                            max: 100,       // Optional: define the maximum value for the scale
+                            beginAtZero: true, // Optional: force the scale to start at zero
+                            display: false
+                        },
+                        // The 'x' axis uses a 'category' scale when labels are provided as strings (common for line/bar charts)
+                        x: {
+                            // For line charts where x-axis data is numeric or time-series, you might use 'linear' or 'time'
+                            type: 'linear',
+                            display: false
+                        }
+                    },
+                    plugins: {
+                        annotation: {
+                            annotations: {
+                                label: {
+                                    type: 'label',
+                                    content: ['Medium Risk'],
+                                    font: { size: 20, family: 'system-ui, Avenir, Helvetica, Arial, sans-serif', weight: 'bolder' },
+                                    color: '#3b82f6',
+                                    backgroundColor: 'transparent',
+                                    xScaleID: 'x',
+                                    yScaleID: 'y',
+                                    xValue: '50%',
+                                    yValue: 10,
+                                    display: true
+                                }
+                            }
+                        }
+                    }
+                }}
+            />
+        </>
+    );
+};
+
+export default GaugeChart;
